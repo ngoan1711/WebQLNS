dd6c9da5bf91523d3253496e001c798857fe0708
DELL <DELL@DUONGTRUONGPHUO>
2024-03-15 20:53:00 +0700

    Add project files.

---
+++ WebQLNS/ThemNhanVien.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace WebQLNS
+{
+    public partial class ThemNhanVien : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btSave_Click(object sender, EventArgs e)
+        {
+            string chuoi_ket_noi = ConfigurationManager.ConnectionStrings["QLNhanVienConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(chuoi_ket_noi);
+            conn.Open();
+            string sql = "insert into nhanvien(honv,tennv,phai,ngaysinh,noisinh,maphong) values (@honv,@tennv,@phai,@ngaysinh,@noisinh,@maphong)";
+            SqlCommand cmd = new SqlCommand(sql,conn);
+            cmd.Parameters.AddWithValue("@honv", txtHo.Text);
+            cmd.Parameters.AddWithValue("@tennv", txtTen.Text);
+            if(rdNam.Checked)
+                cmd.Parameters.AddWithValue("@phai", true);
+            else
+                cmd.Parameters.AddWithValue("@phai", false);
+           
+            cmd.Parameters.AddWithValue("@ngaysinh", txtNgaySinh.Text);
+            cmd.Parameters.AddWithValue("@noisinh", txtNoisinh.Text);
+            cmd.Parameters.AddWithValue("@maphong", ddlPhong.SelectedValue);
+            if (cmd.ExecuteNonQuery() > 0)
+                Response.Redirect("qlNhanVien.aspx");
+            else
+                lbThongBao.Text = "Thao tác thêm nhân viên thất bại";
+        }
+    }
+}
